61f25c44794da5327d69f219b4d95e221203ad2e
Shemely Pérez <111014629+shmcode@users.noreply.github.com>
2023-11-21 15:44:03 -0600

    Update Sintomatologia.java

--- src/main/java/com/uam/CLINICA/model/Sintomatologia.java
+++ src/main/java/com/uam/CLINICA/model/Sintomatologia.java
@@ -4,20 +4,21 @@ import javax.persistence.*;
 
 import lombok.*;
 
-@Embeddable
+
+@Entity
 @Getter
 @Setter
 
 public class Sintomatologia extends Identificable{
-	private String sintoma; //sintoma solo se escriba en minusculas
+	private String nombre; //sintoma solo se escriba en minusculas
 	@PrePersist //Ejecutado justo antes de grabar el objeto por primera vez
 	private void onPersist() throws Exception{
-		this.setSintoma(sintoma.toLowerCase());
+		this.setNombre(nombre.toLowerCase());
 	}
 
 	@PreUpdate //Ejecutado al momento de actualizar posteriormente la entidad
 	private void onUpdate() throws Exception{
-		this.setSintoma(sintoma.toLowerCase());
+		this.setNombre(nombre.toLowerCase());
 	}
 
 }
