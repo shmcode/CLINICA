92ed314122d8328e4846bb88d47c9e3585ff5bba
Shemely Pérez <111014629+shmcode@users.noreply.github.com>
2023-11-21 18:45:37 -0600

    Update DetalleMedicamento.java

--- src/main/java/com/uam/CLINICA/model/DetalleMedicamento.java
+++ src/main/java/com/uam/CLINICA/model/DetalleMedicamento.java
@@ -2,18 +2,35 @@ package com.uam.CLINICA.model;
 
 import java.util.*;
 
+import javax.persistence.*;
+
 import org.openxava.annotations.*;
 
 import lombok.*;
 
-import javax.persistence.*;
-
-@Embeddable
+@Entity
 @Getter @Setter
+@View(name="Simple", members="lote,vencimiento,presentacion,minimoExistencia;"
+		+ "indicaciones;")
 
-public class DetalleMedicamento {
+public class DetalleMedicamento extends Identificable{
+	
+	@ManyToOne(fetch = FetchType.LAZY,
+			optional = true)
+	@DescriptionsList
+	private Medicamento medicamento;
+	
+   private Date lote;
 
-	@OneToOne(fetch = FetchType.LAZY,optional = false)
-	private Inventario inventario;
+    private Date vencimiento;
+
+    private String presentacion;
+
+    @Stereotype("MEMO")
+    private String indicaciones;
+
+    private int minimoExistencia;
+
+	
 }
 
