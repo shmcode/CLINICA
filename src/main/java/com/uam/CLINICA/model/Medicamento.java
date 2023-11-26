ca4584eda50fbea22b917396a688c478f3ee413e
Shemely Pérez <111014629+shmcode@users.noreply.github.com>
2023-11-25 19:24:18 -0600

    MEDICAMENTO ACTUALIZADO

--- src/main/java/com/uam/CLINICA/model/Medicamento.java
+++ src/main/java/com/uam/CLINICA/model/Medicamento.java
@@ -1,32 +1,31 @@
 package com.uam.CLINICA.model;
 
+import java.util.*;
+
 import javax.persistence.*;
 
-import com.uam.CLINICA.Calculadores.ValidadorMed;
 import org.openxava.annotations.*;
 
 import lombok.*;
 
 @Entity
-@Getter
-@Setter
-@NamedQueries({
-        @NamedQuery(name="Medicamento.findByNombre",query="select e from Medicamento e where e.nombre like ?1")
-})
+@Getter @Setter
+@View(name="Simple", members="nombre,nombreGenerico;"
+		+ "cantidadExistencia;"
+		+ "detalleMedicamento;")
 public class Medicamento extends Identificable {
-
-    @PropertyValidator(value= ValidadorMed.class)
-    @Required
-    private String nombre;
 	
-    @Required
+    //@Required
+	private String nombre;
+	
+    //@Required
 	private String nombreGenerico;
-
-    @Required
+    
+    //@Required
 	private Integer cantidadExistencia;
-
-	//@OneToMany
-	//@ReferenceView("Simple")
-	//private Inventario detalleMedicamento;
+	
+	@OneToMany(mappedBy="medicamento", cascade=CascadeType.REMOVE)
+	private List<DetalleMedicamento> detalleMedicamento;
+	
 
 }
